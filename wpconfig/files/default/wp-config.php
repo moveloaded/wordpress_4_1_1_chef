7e9f650bfaae25d132c0ff85395b8cbd5bcd7651
moveloaded <mitch@moveloaded.com>
2015-05-28 16:04:05 -0400

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', 'wordpress.ashvqf.0001.use1.cache.amazonaws.com');
  
-define('WP_HOME','');
-define('WP_SITEURL','');
+define('WP_HOME','wordpress-2121432383.us-east-1.elb.amazonaws.com');
+define('WP_SITEURL','wordpress-2121432383.us-east-1.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
