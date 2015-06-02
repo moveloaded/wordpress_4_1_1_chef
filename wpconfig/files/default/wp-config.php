fe879577ed8bacdf50c679b4886909389b5c97c9
Mitchell Ogden <mitch@moveloaded.com>
2015-06-02 01:48:46 -0400

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -75,6 +75,19 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+/* Multisite */ define( 
+'WP_ALLOW_MULTISITE', true );
+define('MULTISITE', true); 
+define('SUBDOMAIN_INSTALL', 
+false); 
+define('DOMAIN_CURRENT_SITE', 
+'54.88.3.218'); 
+define('PATH_CURRENT_SITE', '/'); 
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+ 
+define( 'SUNRISE', 'on' );
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
