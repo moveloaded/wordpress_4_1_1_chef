55edcfd2dc166e1c01d2ca1eb4a4aa5449b62ad8
Mitchell Ogden <mitch@moveloaded.com>
2015-06-02 02:13:58 -0400

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -79,7 +79,7 @@ define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', 
 false); 
 define('DOMAIN_CURRENT_SITE', 
-'54.88.3.218'); 
+'moveloaded.com'); 
 define('PATH_CURRENT_SITE', '/'); 
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
